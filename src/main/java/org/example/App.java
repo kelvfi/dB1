57d6253f50b1ca5ff5cc271a72fa9b7e31bc1f2e
kelvfi <126086807+kelvfi@users.noreply.github.com>
2023-11-22 09:01:57 +0100

    a

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //INSERT INTO `student` (`id`, `name`, `email`) VALUES (NULL, 'Kelvin Fiegl', 'kelfiegl@tsn.at'), (NULL, 'Claudio Landerer', 'claudio.landerer@myimst.net');
+        System.out.println( "Hello World!" );
+    }
+}
